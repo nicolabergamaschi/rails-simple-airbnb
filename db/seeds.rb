bd7be49f8be0fb0264e933bf58a961087769c45c
nicola bergamaschi <nicola.bergamaschi@gmail.com>
2025-06-17 00:17:27 +0100

    rails-simple-airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+10.times do
+  Flat.create!(
+    name: Faker::Address.street_name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.review,
+    price_per_night: rand(100...400),
+    number_of_guests: rand(1...5)
+  )
+end
